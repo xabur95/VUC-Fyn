d77f1dbc47b4750112c84edf50417bdd1eeffdfd
mpeder75 <mpeder75@gmail.com>
2026-03-17 21:31:09 +0100

    Updated DomainEntity

--- Semesterprojekt1PBA.Domain/DomainEntity.cs
+++ Semesterprojekt1PBA.Domain/DomainEntity.cs
@@ -5,8 +5,62 @@ namespace Semesterprojekt1PBA.Domain;
 ///  Author: Michael
 /// Represents the base type for domain entities with a unique identifier.
 /// </summary>
-public abstract class DomainEntity
+public abstract class DomainEntity : IEquatable<DomainEntity>
 {
     public Guid Id { get; protected set; }
     [Timestamp] public byte[] RowVersion { get; protected set; } = null;
+
+    public bool Equals(DomainEntity? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return Id == other.Id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        if (obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        return Equals((DomainEntity)obj);
+    }
+
+    public static bool operator ==(DomainEntity left, DomainEntity right)
+    {
+        if (left is null)
+        {
+            return right is null;
+        }
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(DomainEntity left, DomainEntity right)
+    {
+        return !(left == right);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
 }
