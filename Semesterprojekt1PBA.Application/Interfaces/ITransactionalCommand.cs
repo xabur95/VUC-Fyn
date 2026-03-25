80b88d8e3cfd226b28a43a96791b64be9d9145ce
xabur <xabor_94@hotmail.com>
2026-03-25 01:20:05 +0100

    just testing

---
+++ Semesterprojekt1PBA.Application/Interfaces/ITransactionalCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Semesterprojekt1PBA.Application.Interfaces
+{
+    public interface ITransactionalCommand
+    {
+    }
+}
