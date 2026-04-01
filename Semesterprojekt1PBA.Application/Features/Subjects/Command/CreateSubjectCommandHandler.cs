6123de8d1e0c7522bf4a225958f6ee7b60caf89a
Mikkel Højer Jensen <mhje49482@edu.ucl.dk>
2026-04-01 17:28:08 +0200

    Added CreateSubjectCommand.cs, CreateSubjectCommandHandler in App. Added ISubjectRepository.cs in Domain.

---
+++ Semesterprojekt1PBA.Application/Features/Subjects/Command/CreateSubjectCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Semesterprojekt1PBA.Domain.Interfaces;
+using Semesterprojekt1PBA.Domain.Entities;
+
+namespace Semesterprojekt1PBA.Application.Features.Subjects.Command
+{
+    public class CreateSubjectCommandHandler : IRequestHandler<CreateSubjectCommand, Guid>
+    {
+        private readonly ISubjectRepository _subjectRepository;
+
+        public CreateSubjectCommandHandler(ISubjectRepository subjectRepository)
+        {
+            _subjectRepository = subjectRepository;
+        }
+
+        public async Task<Guid> Handle(CreateSubjectCommand request, CancellationToken cancellationToken)
+        {
+            Subject subject = Subject.Create(request.Name, request.Level);
+
+            await _subjectRepository.AddAsync(subject);
+
+            return subject.Id;
+        }
+    }
+}
---
+++ Semesterprojekt1PBA.Domain/Interfaces/ISubjectRepository.cs
@@ -0,0 +1,14 @@
+﻿using Semesterprojekt1PBA.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Semesterprojekt1PBA.Domain.Interfaces
+{
+    public interface ISubjectRepository
+    {
+        Task AddAsync(Subject subject);
+        Task<IReadOnlyCollection<Subject>> GetByNameAsyc(string name);
+
+    }
+}
