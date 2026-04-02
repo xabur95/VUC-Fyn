b9725e625e6b5e6e7ce5b82c0d22753f9effb068
Mikkel Højer Jensen <mhje49482@edu.ucl.dk>
2026-04-02 17:16:09 +0200

    Added Create Topic Command with Handler in app layer. Added ITopicRepository in Domain layer.

---
+++ Semesterprojekt1PBA.Domain/Interfaces/ITopicRepository.cs
@@ -0,0 +1,14 @@
+﻿using Semesterprojekt1PBA.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Semesterprojekt1PBA.Domain.Interfaces
+{
+    public interface ITopicRepository
+    {
+        Task AddAsync(Topic topic);
+        Task<IReadOnlyCollection<Topic>> GetTopicsBySubject(Subject subject);
+        Task UpdateAsync(Topic topic);
+    }
+}
