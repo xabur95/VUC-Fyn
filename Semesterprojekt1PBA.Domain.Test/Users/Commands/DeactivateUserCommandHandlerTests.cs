e687665ebfe4ce9f6ff52b0671d87cca3ce93236
Michael Pedersen <mpeder75@gmail.com>
2026-04-03 19:27:21 +0200

    test: test af cqs DONE

---
+++ Semesterprojekt1PBA.Domain.Test/Users/Commands/DeactivateUserCommandHandlerTests.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Moq;
+using Semesterprojekt1PBA.Application.Features.Users.Commands.DeactivateUser;
+using Semesterprojekt1PBA.Domain.Entities;
+using Semesterprojekt1PBA.Domain.Interfaces;
+using Semesterprojekt1PBA.Domain.ValueObjects;
+
+namespace Semesterprojekt1PBA.Domain.Test.Users.Commands;
+
+public class DeactivateUserCommandHandlerTests
+{
+    [Fact]
+    public async Task DeactivateUserCommandHandler_WhenUserIsDeactivated_ReturnsUnit()
+    {
+        // Arrange
+        var mockRepository = new Mock<IUserRepository>();
+        var user = User.Create("Homer", "Simpson", "dooh@gmail.com", RoleType.Student);
+        mockRepository.Setup(repo => repo.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(user);
+        var deactivateUserCommandHandler = new DeactivateUserCommandHandler(mockRepository.Object);
+        var command = new DeactivateUserCommand { Id = user.Id };
+
+        // Act
+        var result = await deactivateUserCommandHandler.Handle(command, CancellationToken.None);
+
+        // Assert
+        Assert.IsType<Unit>(result);
+        mockRepository.Verify(r => r.UpdateAsync(It.IsAny<User>()), Times.Once);
+
+    }
+}
---
+++ Semesterprojekt1PBA.Domain.Test/Users/Commands/UpdateUserCommandHandlerTests.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Moq;
+using Semesterprojekt1PBA.Application.Features.Users.Commands.UpdateUser;
+using Semesterprojekt1PBA.Domain.Entities;
+using Semesterprojekt1PBA.Domain.Interfaces;
+using Semesterprojekt1PBA.Domain.ValueObjects;
+
+namespace Semesterprojekt1PBA.Domain.Test.Users.Commands;
+
+public class UpdateUserCommandHandlerTests
+{
+    [Fact]
+    public async Task UpdateUserCommandHandler_WhenUserIsUpdated_ReturnsUnit()
+    {
+        // Arrange
+        var mockRepository = new Mock<IUserRepository>();
+        var user = User.Create("Homer", "Simpson", "dooh@gmail.com", RoleType.Student);
+        mockRepository.Setup(repo => repo.GetByIdAsync(user.Id)).ReturnsAsync(user);
+        var updateUserCommandHandler = new UpdateUserCommandHandler(mockRepository.Object);
+        var command = new UpdateUserCommand
+        {
+            Id = user.Id,
+            FirstName = "Homer",
+            LastName = "Simpson",
+            Email = "newEmail@gmail.com"
+        };
+
+        // Act
+        var result = await updateUserCommandHandler.Handle(command, CancellationToken.None);
+
+        // Assert
+        Assert.IsType<Unit>(result);
+        mockRepository.Verify(r => r.UpdateAsync(It.IsAny<User>()), Times.Once);
+
+
+    }
+}
