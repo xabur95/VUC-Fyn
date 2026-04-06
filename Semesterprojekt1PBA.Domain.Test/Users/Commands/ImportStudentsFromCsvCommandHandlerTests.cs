6baa637c495372a65721593f9fcf178d27dfb497
mpeder75 <mpeder75@gmail.com>
2026-04-06 12:51:26 +0200

    test: ImportStudentFromCsv i Application

---
+++ Semesterprojekt1PBA.Domain.Test/Users/Commands/ImportStudentsFromCsvCommandHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Moq;
+using Semesterprojekt1PBA.Application.Features.Users.Commands.CreateUser;
+using Semesterprojekt1PBA.Application.Features.Users.Commands.ImportStudents;
+using System.Text;
+
+namespace Semesterprojekt1PBA.Domain.Test.Users.Commands;
+/// <summary>
+/// Author: Michael
+/// Unittests for ImportStudentsFromCsvCommandHandler. Verificerer at handleren korrekt behandler CSV-data
+/// og interagerer med IMediator som forventet.
+/// </summary>
+
+public class ImportStudentsFromCsvCommandHandlerTests
+{
+    [Fact]
+    public async Task Handle_WhenCsvHasOneStudentRow_SendsCreateUserCommandOnce()
+    {
+        // Arrange
+        var csv = "K.nr.;K.id.;Fornavn;Efternavn;Tilmeldt;Ophørt;Pause;Bemærkning;Telefon;Mail;Adresse;Post/By;F.dato\n100001;;Jens;Hansen;23.08.25;;;;12345678;jens@hansen.dk;Bondegaard 4;9999 Landsby; 190210";
+        var mockMediator = new Mock<IMediator>();
+        MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(csv));
+        var importStudentsCommandHandler = new ImportStudentsFromCsvCommandHandler(mockMediator.Object);
+
+        // Act
+        var result = await importStudentsCommandHandler.Handle(new ImportStudentsFromCsvCommand { CsvFile = stream}, CancellationToken.None);
+
+        // Assert
+        mockMediator.Verify(r => r.Send(It.IsAny<CreateUserCommand>(), It.IsAny<CancellationToken>()), Times.Once());
+    }
+}
