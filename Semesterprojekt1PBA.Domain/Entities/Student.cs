36c1e60e67a35260666d2cdb6aba2acd3ebe7667
mpeder75 <mpeder75@gmail.com>
2026-04-14 15:41:49 +0200

    fix: changed Student to have StartDate and EndDate

--- Semesterprojekt1PBA.Domain/Entities/Student.cs
+++ Semesterprojekt1PBA.Domain/Entities/Student.cs
@@ -9,15 +9,15 @@ namespace Semesterprojekt1PBA.Domain.Entities;
 public class Student : User
 {
     public string Knr { get; private set; } = null!;
-    public DateOnly Tilmeldt { get; private set; }
-    public DateOnly? Ophørt { get; private set; }
+    public DateOnly StartDate { get; private set; }
+    public DateOnly? EndDate { get; private set; }
 
-    private Student(string firstName, string lastName, string email, string knr, DateOnly tilmeldt, DateOnly? ophørt)
+    private Student(string firstName, string lastName, string email, string knr, DateOnly startDate, DateOnly? endDate)
         : base(firstName, lastName, email, RoleType.Student)
     {
         Knr = knr;
-        Tilmeldt = tilmeldt;
-        Ophørt = ophørt;
+        StartDate = startDate;
+        EndDate = endDate;
     }
 
     public static Student Create(string firstName, string lastName, string email, string knr, DateOnly tilmeldt, DateOnly? ophørt)
