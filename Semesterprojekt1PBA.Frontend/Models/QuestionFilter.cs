9285121ace93ec85a96562a548957f8405395fec
Mikkel Højer Jensen <mhje49482@edu.ucl.dk>
2026-06-05 12:37:33 +0200

    Currently trying to figure out how to communicate with the API. This commit is mainly to switch machines.

---
+++ Semesterprojekt1PBA.Frontend/Models/QuestionFilter.cs
@@ -0,0 +1,15 @@
+﻿using Semesterprojekt1PBA.Domain.ValueObjectsAndEnums;
+
+namespace Semesterprojekt1PBA.Frontend.Models
+{
+    public class QuestionFilter
+    {
+        public string? Title { get; set; }
+        public string? Text { get; set;  }
+        public int Points { get; set; }
+        public ActiveStatus? ActiveStatus { get; set; }
+        public string? ParentQuestion { get; set; }
+        public string? Answer { get; set; }
+        public int? CreatedByUserId { get; set; }
+    }
+}
